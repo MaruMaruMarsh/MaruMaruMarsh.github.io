653cf47af51ddddaf8ca2bba7ab8e6d513f3793d
MaruMaruMarsh <b0935751611@gmail.com>
2023-05-30 11:21:25 +0800

    Site updated: 2023-05-30 11:21:22

--- js/main.js
+++ js/main.js
@@ -41,4 +41,12 @@ const app = Vue.createApp({
         },
     },
 });
+$(document).on('pjax:start', function () {
+    if (window.aplayers) {
+        for (let i = 0; i < window.aplayers.length; i++) {
+            window.aplayers[i].destroy();
+        }
+        window.aplayers = [];
+    }
+});
 app.mount("#layout");
